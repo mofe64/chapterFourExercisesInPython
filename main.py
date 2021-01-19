b50fa3ce164975b1f85df7a42db0d88f0c5d38aa
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-01-19 01:32:58 +0100

    fixed bug in while loop

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ gas_mileage = GasMileage()
 sentinel = 1
 while sentinel != -1:
     miles_string = input("Enter miles driven ")
-    if int(miles_string == -1):
+    # print(type(miles_string))
+    if int(miles_string) == -1:
         sentinel = -1
         break
     gallons_string = input("Enter gallons used")
-    if int(gallons_string == -1):
+    if int(gallons_string) == -1:
         sentinel = -1
         break
     miles_per_gallon = gas_mileage.calculate_gas_mileage(int(miles_string), int(gallons_string))
     print(f"Miles per gallon is {miles_per_gallon}")
     print(f"Total miles per gallon for all trips so far is: {gas_mileage.get_total_miles_per_gallon()}")
-
